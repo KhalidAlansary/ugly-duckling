9c57727448c0f6873708bfc76c8288c5a91896de
Khalid Alansary <khaled.ansary2003@outlook.com>
2024-12-01 10:44:23 +0200

    chore: add docstrings in parser.hpp

--- include/parser.hpp
+++ include/parser.hpp
@@ -6,7 +6,15 @@
 #include <tuple>
 #include <unordered_map>
 
-// Represents an XML node.
+/**
+ * @brief Constructs an XMLNode.
+ *
+ * @param tag_name The name of the tag (required).
+ * @param parent The parent node (optional).
+ * @param children The list of child nodes (optional).
+ * @param content The content of the node (optional).
+ * @param attributes The attributes of the node (optional).
+ */
 class XMLNode {
  public:
   const std::string tag_name;
@@ -20,10 +28,33 @@ class XMLNode {
           const std::list<XMLNode>& children = {},
           const std::string& content = "",
           const std::unordered_map<std::string, std::string>& attributes = {});
+
+  /**
+   * @brief Returns the opening tag in angle brackets.
+   *
+   * For example, if the tag name is "h", it returns "<h>".
+   *
+   * @return The opening tag as a string.
+   */
   std::string get_opening_tag() const;
+
+  /**
+   * @brief Returns the closing tag in angle brackets.
+   *
+   * For example, if the tag name is "h", it returns "</h>".
+   *
+   * @return The closing tag as a string.
+   */
   std::string get_closing_tag() const;
 };
 
+/**
+ * @brief Parses an XML string into an XMLNode tree.
+ *
+ * @param xml The XML string to parse.
+ * @return A tuple containing the root XMLNode and a boolean indicating whether
+ * the original string was valid.
+ */
 std::tuple<XMLNode, bool> parse_xml(const std::string& xml);
 
 #endif
