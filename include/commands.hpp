7ec4d83fb35bb9e2be87d7477213f3e398ffa548
fkamalo <farahhkamalo@gmail.com>
2024-12-26 18:13:40 +0200

    uncommenting necessary lines

--- include/commands.hpp
+++ include/commands.hpp
@@ -3,8 +3,8 @@
 
 int verify(int argc, char* argv[]);
 // TODO: Uncomment the following lines after implementing the functions
-// int format(int argc, char* argv[]);
-// int json(int argc, char* argv[]);
+ int format(int argc, char* argv[]);
+ int json(int argc, char* argv[]);
 // int mini(int argc, char* argv[]);
 // int compress(int argc, char* argv[]);
 // int decompress(int argc, char* argv[]);
