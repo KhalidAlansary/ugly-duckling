b10555b438eab9a9f2187e1a1e5894c598e61117
Khalid Alansary <khaled.ansary2003@outlook.com>
2024-12-27 19:46:24 +0200

    chore: move includes to the correct location

--- include/compressor.hpp
+++ include/compressor.hpp
@@ -2,20 +2,13 @@
 #define COMPRESSOR_HPP
 
 #include <string>
+
 #include "parser.hpp"
-#include <vector>
-#include <functional>
-#include <sstream>
-#include "minifier.hpp"
-#include <boost/program_options.hpp>
-#include <fstream>
-#include <iostream>
-namespace po = boost::program_options;
 
 /**
  * @brief Compresses the input XML string.
  *
- * This function parses the XML string into a tree, compresses the tags, 
+ * This function parses the XML string into a tree, compresses the tags,
  * and then minifies the resulting tree back into a string.
  *
  * @param xml The XML string to be compressed.
@@ -27,7 +20,8 @@ std::string compress_xml(const std::string& xml);
 /**
  * @brief Compresses the XML tree in place.
  *
- * This function modifies the provided tree by replacing tag names with compressed versions.
+ * This function modifies the provided tree by replacing tag names with
+ * compressed versions.
  *
  * @param root The root of the ElementNode tree.
  */
--- include/decompressor.hpp
+++ include/decompressor.hpp
@@ -2,22 +2,14 @@
 #define DECOMPRESSOR_HPP
 
 #include <string>
+
 #include "parser.hpp"
-#include <vector>
-#include "minifier.hpp"
-#include <functional>
-#include <sstream>  
-#include <boost/program_options.hpp>
-#include <fstream>
-#include <iostream>
-#include "decompressor.hpp"
-namespace po = boost::program_options;
 
 /**
  * @brief Decompresses the input XML string.
  *
- * This function parses the compressed XML string into a tree, decompresses the tags, 
- * and then minifies the resulting tree back into a string.
+ * This function parses the compressed XML string into a tree, decompresses the
+ * tags, and then minifies the resulting tree back into a string.
  *
  * @param xml The XML string to be decompressed.
  * @return A decompressed XML string.
@@ -28,19 +20,19 @@ std::string decompress_xml(const std::string& xml);
 /**
  * @brief decompresses the XML tree in place.
  *
- * This function modifies the provided tree by replacing compresssed tag names with decompressed versions.
+ * This function modifies the provided tree by replacing compresssed tag names
+ * with decompressed versions.
  *
  * @param root The root of the ElementNode tree.
  */
-
 
 void decompress_tree_in_place(ElementNode& root);
 
 /**
  * @brief Main function to compress the XML from command-line arguments.
  *
- * This function reads the compressed XML file specified in the command line arguments,
- * decompresses it, and prints the decompressed result.
+ * This function reads the compressed XML file specified in the command line
+ * arguments, decompresses it, and prints the decompressed result.
  */
 
 int decompress(int argc, char* argv[]);
