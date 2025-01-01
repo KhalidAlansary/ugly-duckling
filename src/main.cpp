057e1911c814cad6f6834f4da1c02ed9b03ef08e
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-01-01 16:02:09 +0200

    fix: help message again

--- src/main.cpp
+++ src/main.cpp
@@ -42,11 +42,11 @@ int main(int argc, char* argv[]) {
         "  draw              Draw the XML file\n"
         "  most_active       Write the user with most connections\n"
         "  most_influencer   Write the most followed username and id\n"
-        "  mutual            Writes a list of the mutual users between users "
+        "  mutual            Write a list of the mutual users between users "
         "with given ids\n"
-        "  suggest           Writes a list of suggested users for user with a "
+        "  suggest           Write a list of suggested users for user with a "
         "given id \n"
-        "  search            Writes a list of posts where the given word or "
+        "  search            Write a list of posts where the given word or "
         "topic was mentioned ");
 
     po::positional_options_description p;
